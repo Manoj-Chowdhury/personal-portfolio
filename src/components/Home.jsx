a385814265f9e312fdb2f6076efe56bc954a986d
Manoj Chowdhury <manojchowdhury599@gmail.com>
2023-08-04 12:02:23 +0530

    change in title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full mt-32 md:mt-0">
           <h2 className="text-4xl md:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Full Stack mern Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             Full Stack MERN Developer with experience in building and designing softwares. Currently, I love to work on web application using technologies like React, Tailwind, Express.js, Node.js, MongoDB.
